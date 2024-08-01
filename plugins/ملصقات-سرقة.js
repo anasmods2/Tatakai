df8ba296859e3f2cf4281cc8901f7249fb0c74b3
anasmods2 <155864003+anasmods2@users.noreply.github.com>
2024-08-02 01:33:55 +0300

    Add files via upload

---
+++ plugins/ملصقات-سرقة.js
@@ -0,0 +1,23 @@
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!m.quoted) throw `${mg} الرد على الملصق بالوصف والاسم\nمثل\n*${usedPrefix + command} النص1|النص2*`
+let stiker = false 
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw `${mg} الرد على الملصق بالوصف والاسم\nمثل\n*${usedPrefix + command} النص1|النص2*`
+let img = await m.quoted.download()
+if (!img) throw `${mg} الرد على الملصق بالوصف والاسم\nمثل\n*${usedPrefix + command} النص1|النص2*`
+stiker = await addExif(img, packname || 'ΨᎪᎢᎾ ᏴᎾᎢ', author || '')
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw `${fg} الرد على الملصق بالوصف والاسم\nمثل\n*${usedPrefix + command} النص1|النص2*`
+}}
+handler.help = ['wm <packname>|<author>']
+handler.tags = ['sticker']
+handler.command = /^wm|سرقه|حقوق|سرقة$/i
+export default handler
